bd139b8ec4793a91d7193cce8a91c4dbf62764c2
mr1-bit <bj.bakker08@gmail.com>
2025-02-26 11:10:13 +0100

    removed 1 usless comment

--- site/functions.js
+++ site/functions.js
@@ -1,11 +1,4 @@
 let shop = document.getElementById("shop");
-
-// let shopitemsdata = [{id:"img", name: "geko", price: 45, desc: "nice one with plenty of color", img: "MainBefore.jpg"},
-//     {id:"img2", name: "johan", price: 15, desc: "nice one with plenuiovusiity of color", img: "MainBefore.jpg"},
-//     {id:"img3", name: "nope", price: 20, desc: "nice one got color asdasdadasdasd", img: "MainBefore.jpg"},
-//     {id:"img4", name: "geko2", price: 25, desc: "nice one with pleksfdjnty of color", img: "MainBefore.jpg"}
-// ];
-
 let basket = JSON.parse(localStorage.getItem("data")) || []
 
 let generate = () => {
